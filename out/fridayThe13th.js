adad59cc71ec089748ff57933bcf132d4b4344a2
tinteto <radosttina.dimitrova@gmail.com>
2025-05-14 16:22:49 +0300

    Problem: Friday the 13th

---
+++ out/fridayThe13th.js
@@ -0,0 +1,29 @@
+"use strict";
+function getDate(data) {
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    for (let item of data) {
+        const date = new Date(item);
+        let isValidDate = !isNaN(date.getTime());
+        if (date instanceof Date &&
+            isValidDate &&
+            date.getDate() === 13 &&
+            date.getDay() === 5) {
+            const day = date.getDate();
+            const month = months[date.getMonth()];
+            const year = date.getFullYear();
+            console.log(`${day}-${month}-${year}`);
+        }
+    }
+}
+getDate([
+    {},
+    new Date(2025, 4, 13),
+    null,
+    new Date(2025, 5, 13),
+    '13-09-2023',
+    new Date(2025, 6, 13),
+]);
+//# sourceMappingURL=fridayThe13th.js.map
